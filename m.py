bb970628ae23c3e03abdaac22408801ba1df5f3f
Ttt <115389869+Rotjocker@users.noreply.github.com>
2023-09-28 18:36:17 +0300

    Update m.py

--- m.py
+++ m.py
@@ -3,7 +3,7 @@ from telethon import TelegramClient
 from telethon import TelegramClient, events, sync
 from telethon.tl.functions.messages import GetHistoryRequest, GetBotCallbackAnswerRequest
 print("</>  </> ")
-ch = "@Open_CC_Bot"
+ch = "@JOKER_CHKBOT"
 api_id = '15372008'
 api_hash = 'ed85c35370cffb1e2cf050e44e635d26'
 client = TelegramClient('session', api_id, api_hash)
@@ -11,7 +11,7 @@ client.start()
 for cc in open("F4.txt").read().split("\n"):
     try:
         client.send_message(ch ,f"/chk {cc}")
-        time.sleep(random.randint(10,16))
+        time.sleep(random.randint(10,12))
         mssag = client.get_messages(ch, limit=1)
         if "ANTI_SPAM" in str(mssag[0].message):
             t = str(mssag[0].message).split("again after ")[1]
